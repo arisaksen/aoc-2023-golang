3e2b380f55e618e606829877f96a241ca663c874
Arne Ruben Isaksen <arisaksen@gmail.com>
2024-07-04 17:24:32 +0200

    day01

--- day01/day01.go
+++ day01/day01.go
@@ -3,17 +3,33 @@ package main
 import (
 	utils "aoc/golang"
 	"fmt"
+	"strings"
 	"unicode"
 )
 
 func main() {
-	//puzzle1Input := utils.ReadLines("day01/puzzle1.txt")
-	//fmt.Println("Part 1: ", getSeeds(puzzle1Input))
-	//
+
+	//part1()
+
+	part2()
+
+}
+func part1() {
 	puzzle1Input := utils.ReadLines("day01/puzzle1.txt")
 	calibrations := getCalibrations(puzzle1Input)
 	sumCalibrations := sumUpCalibrations(calibrations)
 	fmt.Println("Part 1: ", sumCalibrations)
+}
+
+func part2() {
+	puzzle2Input := utils.ReadLines("day01/puzzle2.txt")
+	var calibrationsEdited []string
+	for _, line := range puzzle2Input {
+		calibrationsEdited = append(calibrationsEdited, addNumbersBeforeWords(line))
+	}
+	calibrations := getCalibrations(calibrationsEdited)
+	sumCalibrations := sumUpCalibrations(calibrations)
+	fmt.Println("Part 2: ", sumCalibrations)
 }
 
 func getCalibrations(lines []string) []int {
@@ -45,6 +61,40 @@ func getCalibrations(lines []string) []int {
 	}
 
 	return calibrations
+}
+
+func addNumbersBeforeWords(input string) string {
+	var spelledDigits = map[string]string{
+		"one":   "1",
+		"two":   "2",
+		"three": "3",
+		"four":  "4",
+		"five":  "5",
+		"six":   "6",
+		"seven": "7",
+		"eight": "8",
+		"nine":  "9",
+	}
+	var result strings.Builder
+	i := 0
+	length := len(input)
+
+	for i < length {
+		matchFound := false
+		for word, digit := range spelledDigits {
+			if strings.HasPrefix(input[i:], word) {
+				result.WriteString(digit + word)
+				i += len(word)
+				matchFound = true
+				break
+			}
+		}
+		if !matchFound {
+			result.WriteByte(input[i])
+			i++
+		}
+	}
+	return result.String()
 }
 
 func sumUpCalibrations(calibrations []int) int {
--- day01/day01_test.go
+++ day01/day01_test.go
@@ -6,19 +6,46 @@ import (
 	"testing"
 )
 
-func TestCalibrations(t *testing.T) {
+func TestPart1(t *testing.T) {
+	exampleLines := utils.ReadLines("example1.txt")
 
-	t.Run("Part 1", func(t *testing.T) {
-		exampleLines := utils.ReadLines("example1.txt")
+	t.Run("getCalibrations", func(t *testing.T) {
 		expected := []int{12, 38, 15, 77}
 		actual := getCalibrations(exampleLines)
 		assert.Equal(t, expected, actual)
 	})
 
-	t.Run("Part 1", func(t *testing.T) {
-		exampleLines := utils.ReadLines("example1.txt")
+	t.Run("sumUpCalibrations", func(t *testing.T) {
 		expected := 142
 		actual := sumUpCalibrations(getCalibrations(exampleLines))
+		assert.Equal(t, expected, actual)
+	})
+
+}
+
+func TestPart2(t *testing.T) {
+	exampleLines := "two1ninetwo"
+
+	t.Run("getNumbersFromText", func(t *testing.T) {
+		expected := "2two19nine2two"
+		actual := addNumbersBeforeWords(exampleLines)
+		assert.Equal(t, expected, actual)
+	})
+
+	exampleLines2 := utils.ReadLines("example2.txt")
+	var calibrationsEdited []string
+	for _, line := range exampleLines2 {
+		calibrationsEdited = append(calibrationsEdited, addNumbersBeforeWords(line))
+	}
+	t.Run("getCalibrationsPart2", func(t *testing.T) {
+		expected := []int{29, 83, 13, 24, 42, 14, 76}
+		actual := getCalibrations(calibrationsEdited)
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("sumUpCalibrations", func(t *testing.T) {
+		expected := 281
+		actual := sumUpCalibrations(getCalibrations(calibrationsEdited))
 		assert.Equal(t, expected, actual)
 	})
 
