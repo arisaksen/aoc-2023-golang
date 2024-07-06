ce73ced52d4e923101b475c7e9a179fdd8ef3c51
Arne Ruben Isaksen <arisaksen@gmail.com>
2024-07-06 02:14:50 +0200

    Day05 part1

--- day05/day05.go
+++ day05/day05.go
@@ -3,6 +3,9 @@ package main
 import (
 	utils "aoc/golang"
 	"fmt"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func main() {
@@ -11,20 +14,96 @@ func main() {
 }
 
 func part1(filename string) int {
-	puzzle2Input := utils.ReadLines(filename)
+	start := time.Now()
 
-	seeds := getSeeds(puzzle2Input[0])
-	fmt.Println(seeds)
+	puzzle2Input := utils.ReadText(filename)
+	categoryMap := createMap(puzzle2Input)
+	seeds := categoryMap["seeds"][0]
 
-	for _, line := range puzzle2Input {
-		fmt.Println(line)
+	soil := destinationToSource(seeds, categoryMap["seed-to-soil"])
+	fertilizer := destinationToSource(soil, categoryMap["soil-to-fertilizer"])
+	water := destinationToSource(fertilizer, categoryMap["fertilizer-to-water"])
+	light := destinationToSource(water, categoryMap["water-to-light"])
+	temp := destinationToSource(light, categoryMap["light-to-temperature"])
+	humidity := destinationToSource(temp, categoryMap["temperature-to-humidity"])
+	location := destinationToSource(humidity, categoryMap["humidity-to-location"])
+
+	var lowestNumber int
+	for _, number := range location {
+		if number < lowestNumber || lowestNumber == 0 {
+			lowestNumber = number
+		}
 	}
 
-	return 1 * 2 * 3
+	duration := time.Since(start)
+	fmt.Print("Execution time: ")
+	fmt.Println(duration)
+
+	return lowestNumber
 }
 
-func getSeeds(seeds string) string {
-	var x = ""
+func destinationToSource(values []int, toMaps [][]int) []int {
+	var correspondingNumbers []int
+	for _, value := range values {
 
-	return x
+		noValueSet := true
+		var correspondingNumber int
+		for _, toMap := range toMaps {
+			destinationRangeStart := toMap[0]
+			sourceRangeStart := toMap[1]
+			rangeLength := toMap[2]
+
+			//destinationRangeEnd := destinationRangeStart + rangeLength
+			sourceRangeEnd := sourceRangeStart + rangeLength
+
+			if value >= sourceRangeStart && value < sourceRangeEnd {
+				correspondingNumber = value + destinationRangeStart - sourceRangeStart
+				noValueSet = false
+			}
+		}
+		if noValueSet {
+			correspondingNumber = value
+		}
+		correspondingNumbers = append(correspondingNumbers, correspondingNumber)
+	}
+
+	return correspondingNumbers
+}
+
+func createMap(input string) map[string][][]int {
+	lines := strings.Split(input, "\n\n")
+
+	var categoryMap = make(map[string][][]int)
+	for _, line := range lines {
+		splitString := strings.Split(line, ":")
+		key := strings.TrimSuffix(splitString[0], " map")
+		value := cleanValue(splitString[1])
+
+		categoryMap[key] = value
+	}
+
+	return categoryMap
+}
+
+func cleanValue(values string) [][]int {
+	valuesTrimmedFirstSpace := strings.TrimPrefix(values, " ")
+	valuesTrimmedFirstNewline := strings.TrimPrefix(valuesTrimmedFirstSpace, "\n")
+	valuesSplitAtNewLine := strings.Split(valuesTrimmedFirstNewline, "\n")
+
+	var intValuesList [][]int
+	for _, line := range valuesSplitAtNewLine {
+		valuesSplitAtSpace := strings.Split(line, " ")
+
+		var intValues []int
+		for _, stringValue := range valuesSplitAtSpace {
+			valueAsInt, err := strconv.Atoi(stringValue)
+			if err != nil {
+				panic("Value '" + stringValue + "' can not be converted to int")
+			}
+			intValues = append(intValues, valueAsInt)
+		}
+		intValuesList = append(intValuesList, intValues)
+	}
+
+	return intValuesList
 }
--- day05/day05_test.go
+++ day05/day05_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	utils "aoc/golang"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -16,23 +17,81 @@ func TestPart1(t *testing.T) {
 
 }
 
-func TestSeeds(t *testing.T) {
+func TestMaps(t *testing.T) {
 
-	t.Run("Part 1", func(t *testing.T) {
-		expected := map[string][]int{"seeds": {1, 2, 3, 4}}
-		actual := getSeeds("seeds: 1 2 3 4")
+	t.Run("getSeedsFromMap", func(t *testing.T) {
+		exampleInput := utils.ReadText("example.txt")
+		categoryMap := createMap(exampleInput)
 
+		expected := [][]int{{79, 14, 55, 13}}
+		actual := categoryMap["seeds"]
 		assert.Equal(t, expected, actual)
+
+		expectedStF := [][]int{{0, 15, 37}, {37, 52, 2}, {39, 0, 15}}
+		actualStf := categoryMap["soil-to-fertilizer"]
+		assert.Equal(t, expectedStF, actualStf)
 	})
 
 }
 
-//func TestPart2(t *testing.T) {
-//
-//	t.Run("Part 2", func(t *testing.T) {
-//		expected := 71503
-//		actual := part2("test-input.txt")
-//		assert.Equal(t, expected, actual)
-//	})
-//
-//}
+func TestMapping(t *testing.T) {
+	exampleInput := utils.ReadText("example.txt")
+	categoryMap := createMap(exampleInput)
+
+	t.Run("seedToSoil", func(t *testing.T) {
+		seedsMap := categoryMap["seeds"][0]
+		seedToSoilMap := categoryMap["seed-to-soil"]
+		expected := []int{81, 14, 57, 13}
+		actual := destinationToSource(seedsMap, seedToSoilMap)
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("soilToFertilizer", func(t *testing.T) {
+		soil := []int{81, 14, 57, 13}
+		soilToFertMap := categoryMap["soil-to-fertilizer"]
+		expected := []int{81, 53, 57, 52}
+		actual := destinationToSource(soil, soilToFertMap)
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("fertToWater", func(t *testing.T) {
+		fert := []int{81, 53, 57, 52}
+		fertToWater := categoryMap["fertilizer-to-water"]
+		expected := []int{81, 49, 53, 41}
+		actual := destinationToSource(fert, fertToWater)
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("waterToLight", func(t *testing.T) {
+		water := []int{81, 49, 53, 41}
+		waterToLight := categoryMap["water-to-light"]
+		expected := []int{74, 42, 46, 34}
+		actual := destinationToSource(water, waterToLight)
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("lightToTemp", func(t *testing.T) {
+		light := []int{74, 42, 46, 34}
+		lightToTemp := categoryMap["light-to-temperature"]
+		expected := []int{78, 42, 82, 34}
+		actual := destinationToSource(light, lightToTemp)
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("tempToHumidity", func(t *testing.T) {
+		temp := []int{78, 42, 82, 34}
+		tempToHumidity := categoryMap["temperature-to-humidity"]
+		expected := []int{78, 43, 82, 35}
+		actual := destinationToSource(temp, tempToHumidity)
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("humidityToLocation", func(t *testing.T) {
+		humidity := []int{78, 43, 82, 35}
+		humidityToLocation := categoryMap["humidity-to-location"]
+		expected := []int{82, 43, 86, 35}
+		actual := destinationToSource(humidity, humidityToLocation)
+		assert.Equal(t, expected, actual)
+	})
+
+}
