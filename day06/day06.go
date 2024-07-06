979490b16fcb70006d1e770ced0978108f693ee1
Arne Ruben Isaksen <arisaksen@gmail.com>
2024-07-06 14:23:14 +0200

    Day06 both parts solved

--- day06/day06.go
+++ day06/day06.go
@@ -3,19 +3,81 @@ package main
 import (
 	utils "aoc/golang"
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Part 1: ", part1("day05/puzzle1.txt"))
-
+	fmt.Println("Part 1: ", part1("day06/puzzle1.txt"))
+	fmt.Println("Part 2: ", part2("day06/puzzle1.txt"))
 }
 
 func part1(filename string) int {
-	puzzle2Input := utils.ReadLines(filename)
+	puzzle1Input := utils.ReadLines(filename)
 
-	for _, line := range puzzle2Input {
-		fmt.Println(line)
+	times := extractNumbers(puzzle1Input[0])
+	distances := extractNumbers(puzzle1Input[1])
+
+	var numberOfWaysToWinPerRace []int
+	for i, time := range times {
+		numberOfWaysToWinPerRace = append(numberOfWaysToWinPerRace, numberOfWins(time, distances[i]))
 	}
 
-	return 1 * 2 * 3
+	var winWaysMultiplied = 1
+	for _, number := range numberOfWaysToWinPerRace {
+		winWaysMultiplied = winWaysMultiplied * number
+	}
+
+	return winWaysMultiplied
+}
+
+func part2(filename string) int {
+	puzzle2Input := utils.ReadLines(filename)
+
+	time := extractNumber(puzzle2Input[0])
+	distance := extractNumber(puzzle2Input[1])
+
+	return numberOfWins(time, distance)
+}
+
+func extractNumber(input string) int {
+	removeLabel := strings.Split(input, ":")[1]
+	removeSpaces := strings.ReplaceAll(removeLabel, " ", "")
+	number, _ := strconv.Atoi(removeSpaces)
+
+	return number
+}
+
+func extractNumbers(input string) []int {
+	// Split the string by whitespace
+	parts := strings.Fields(input)
+
+	// The first part is the label, so we discard it
+	parts = parts[1:]
+
+	var numbers []int
+	for _, part := range parts {
+		number, _ := strconv.Atoi(part)
+		numbers = append(numbers, number)
+	}
+	return numbers
+}
+
+func numberOfWins(time int, distance int) int {
+	timeToBeat := time
+	maxHoldTime := time
+
+	var waysToWin []int
+	for holdTime := 1; holdTime < maxHoldTime; holdTime++ {
+		speed := holdTime
+		raceTime := distance / speed
+		totalRaceTime := raceTime + holdTime
+		if totalRaceTime < timeToBeat {
+			waysToWin = append(waysToWin, holdTime)
+		}
+		//fmt.Printf("timeToBeat: %d, totalRaceTime: %d", timeToBeat, totalRaceTime)
+		//fmt.Println()
+	}
+
+	return len(waysToWin)
 }
--- day06/day06_test.go
+++ day06/day06_test.go
@@ -6,22 +6,57 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPart1(t *testing.T) {
+func TestPart(t *testing.T) {
 
 	t.Run("Part 1", func(t *testing.T) {
-		expected := 35
+		expected := 288
 		actual := part1("example.txt")
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("Part 2", func(t *testing.T) {
+		expected := 71503
+		actual := part2("example.txt")
 		assert.Equal(t, expected, actual)
 	})
 
 }
 
-//func TestPart2(t *testing.T) {
-//
-//	t.Run("Part 2", func(t *testing.T) {
-//		expected := 71503
-//		actual := part2("test-input.txt")
-//		assert.Equal(t, expected, actual)
-//	})
-//
-//}
+func TestUnits(t *testing.T) {
+
+	t.Run("numberOfWins7", func(t *testing.T) {
+		time := 7
+		distance := 9
+
+		expected := 4
+		actual := numberOfWins(time, distance)
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("numberOfWins15", func(t *testing.T) {
+		time := 15
+		distance := 40
+
+		expected := 8
+		actual := numberOfWins(time, distance)
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("numberOfWins30", func(t *testing.T) {
+		time := 30
+		distance := 200
+
+		expected := 9
+		actual := numberOfWins(time, distance)
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("test", func(t *testing.T) {
+		input := "Time:      7  15   30"
+
+		expected := 71530
+		actual := extractNumber(input)
+		assert.Equal(t, expected, actual)
+	})
+
+}
